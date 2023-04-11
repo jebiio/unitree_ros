b39bdded286717e1f663148859d05429825be7be
yslee <ptss37@gmail.com>
2023-04-11 11:22:16 +0900

    add unitree_teleop

--- unitree_controller/src/unitree_teleop.cpp
+++ unitree_controller/src/unitree_teleop.cpp
@@ -6,10 +6,8 @@
 #include <termios.h>
 
 #include <ros/ros.h>
-#include <geometry_msgs/Wrench.h>
-#include <gazebo_msgs/ModelState.h>
-#include <gazebo_msgs/ModelStates.h>
 #include <gazebo_msgs/SetModelState.h>
+#include <gazebo_msgs/GetModelState.h>
 #include <tf/transform_datatypes.h>
 
 #define KEYCODE_UP 0x41
@@ -21,83 +19,84 @@
 int kfd = 0;
 struct termios cooked, raw;
 
-class unitreeTeleop
+class UnitreeTeleop
 {
 public:
-    unitreeTeleop();
+    UnitreeTeleop();
     void keyLoop();
     void pubForce(double x, double y, double z, double ax, double ay, double az);
-    void states_cb(const gazebo_msgs::ModelStates::ConstPtr &msg);
 
 private:
     double x, y, z, ax, ay, az;
-    ros::NodeHandle n;
-    ros::Publisher force_pub;
-    geometry_msgs::Wrench Force;
-    gazebo_msgs::ModelState model_state_pub;
-    ros::Publisher move_publisher;
-    ros::Subscriber state_sub;
+    std::string robot_name;
+    ros::NodeHandle nh_;
+    ros::ServiceClient set_model_state_client_;
+    ros::ServiceClient get_model_state_client_;
 };
 
-unitreeTeleop::unitreeTeleop()
+UnitreeTeleop::UnitreeTeleop()
 {
-    state_sub = n.subscribe("/gazebo/model_states", 1000, &unitreeTeleop::states_cb, this);
-    move_publisher = n.advertise<gazebo_msgs::ModelState>("/gazebo/set_model_state", 1000);
-    std::string robot_name;
+    set_model_state_client_ = nh_.serviceClient<gazebo_msgs::SetModelState>("/gazebo/set_model_state");
+    get_model_state_client_ = nh_.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state");
+    
     ros::param::get("/robot_name", robot_name);
-    std::cout << "robot_name: " << robot_name << std::endl;
+    robot_name = robot_name + "_gazebo";
 
-    model_state_pub.model_name = robot_name + "_gazebo";
     ros::Rate loop_rate(30);
 }
 
-void quit(int sig)
+void UnitreeTeleop::pubForce(double x, double y, double z, double ax, double ay, double az)
 {
-    tcsetattr(kfd, TCSANOW, &cooked);
-    ros::shutdown();
-    exit(0);
-}
+    gazebo_msgs::GetModelState get_model_state;
+    get_model_state.request.model_name = robot_name;
+    if (!get_model_state_client_.call(get_model_state))
+    {
+        ROS_ERROR("Failed to call service /gazebo/get_model_state");
+        return;
+    }
 
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "my_teleop");
-    unitreeTeleop teleop;
-    signal(SIGINT, quit);
-    teleop.keyLoop();
-    return (0);
-}
+    tf::Quaternion q(
+        get_model_state.response.pose.orientation.x,
+        get_model_state.response.pose.orientation.y,
+        get_model_state.response.pose.orientation.z,
+        get_model_state.response.pose.orientation.w);
 
-void unitreeTeleop::pubForce(double x, double y, double z, double ax, double ay, double az)
-{
-    
-    model_state_pub.twist.linear.x = x; // 0.02: 2cm/sec
-    model_state_pub.twist.linear.y = y;
-    model_state_pub.twist.linear.z = z;
+    double roll, pitch, yaw;
+    tf::Matrix3x3(q).getRPY(roll, pitch, yaw);
+    tf::Quaternion q_stand;
+    q_stand.setRPY(0, 0, yaw);
 
-    model_state_pub.twist.angular.x = ax;
-    model_state_pub.twist.angular.y = ay;
-    model_state_pub.twist.angular.z = az;
+    gazebo_msgs::SetModelState set_model_stand;
+    set_model_stand.request.model_state.model_name = robot_name;
+    set_model_stand.request.model_state.pose = get_model_state.response.pose;
+    set_model_stand.request.model_state.pose.orientation.x = q_stand.getX();
+    set_model_stand.request.model_state.pose.orientation.y = q_stand.getY();
+    set_model_stand.request.model_state.pose.orientation.z = q_stand.getZ();
+    set_model_stand.request.model_state.pose.orientation.w = q_stand.getW();
 
-    model_state_pub.reference_frame = "base";
+    if (!set_model_state_client_.call(set_model_stand))
+    {
+        ROS_ERROR("Failed to call service /gazebo/set_model_state");
+        return;
+    }
 
-    move_publisher.publish(model_state_pub);
+    gazebo_msgs::SetModelState set_model_state;
+    set_model_state.request.model_state.model_name = "laikago_gazebo";
+    set_model_state.request.model_state.reference_frame = "base";
+    // set_model_state.request.model_state.pose = get_model_state.response.pose;
+    set_model_state.request.model_state.twist.linear.x = x;
+    set_model_state.request.model_state.twist.linear.y = 0;
+    set_model_state.request.model_state.twist.angular.z = az;
+
+    if (!set_model_state_client_.call(set_model_state))
+    {
+        ROS_ERROR("Failed to call service /gazebo/set_model_state");
+        return;
+    }
     ros::spinOnce();
 }
 
-void unitreeTeleop::states_cb(const gazebo_msgs::ModelStates::ConstPtr &msg)
-{
-    tf::Quaternion q(msg->pose[2].orientation.x, msg->pose[2].orientation.y, msg->pose[2].orientation.z, msg->pose[2].orientation.w);
-    double roll, pitch, yaw;
-    tf::Matrix3x3(q).getRPY(roll, pitch, yaw);
-    tf::Quaternion q_zero_roll_pitch = tf::createQuaternionFromRPY(0, 0, yaw);
-
-    model_state_pub.pose.orientation.x = q_zero_roll_pitch.getX();
-    model_state_pub.pose.orientation.y = q_zero_roll_pitch.getY();
-    model_state_pub.pose.orientation.z = q_zero_roll_pitch.getZ();
-    model_state_pub.pose.orientation.w = q_zero_roll_pitch.getW();
-}
-
-void unitreeTeleop::keyLoop()
+void UnitreeTeleop::keyLoop()
 {
     char c;
     bool dirty = false;
@@ -124,7 +123,7 @@ void unitreeTeleop::keyLoop()
         switch (c)
         {
         case KEYCODE_UP:
-            x = 1;
+            x = 2.0;
             y = 0.0;
             z = 0.0;
             ax = 0.0;
@@ -133,7 +132,7 @@ void unitreeTeleop::keyLoop()
             dirty = true;
             break;
         case KEYCODE_DOWN:
-            x = -1;
+            x = -2.0;
             y = 0.0;
             z = 0.0;
             ax = 0.0;
@@ -147,7 +146,7 @@ void unitreeTeleop::keyLoop()
             z = 0.0;
             ax = 0.0;
             ay = 0.0;
-            az = 1.0;
+            az = 3.0;
             dirty = true;
             break;
         case KEYCODE_RIGHT:
@@ -156,7 +155,7 @@ void unitreeTeleop::keyLoop()
             z = 0.0;
             ax = 0.0;
             ay = 0.0;
-            az = -1.0;
+            az = -3.0;
             dirty = true;
             break;
         case KEYCODE_SPACE:
@@ -173,3 +172,24 @@ void unitreeTeleop::keyLoop()
     }
     return;
 }
+
+void quit(int sig)
+{
+    tcsetattr(kfd, TCSANOW, &cooked);
+    ros::shutdown();
+    exit(0);
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "my_teleop");
+
+    UnitreeTeleop teleop;
+
+    signal(SIGINT, quit);
+
+    teleop.keyLoop();
+
+    return (0);
+}
+
